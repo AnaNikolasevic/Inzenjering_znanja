76e2f640b4c1b27a73d8b8bd5e5be7fcd6756770
ananikolasevic <23nikolasevic@gmail.com>
2020-04-22 15:10:13 +0200

    added new diseases and anamnesis

--- Rule_Based/diagnosis.pl
+++ Rule_Based/diagnosis.pl
@@ -1,3 +1,6 @@
+% DIAGNOSIS
+%-----------
+
 diagnosis(X,  gallstone):- (cbc(X, high_wbc); liver_panel(X, high_bilirubin); liver_panel(X, high_ALP); lipase(X, high_pancreatic_enzymes)), 
 			  	   (mri(X, gallstone); ct(X, gallstone); ercp(X, gallstone); ultrasound(X, gallstone)).
 
@@ -15,4 +18,15 @@ diagnosis(X, pancreatic_cancer):- (c19_9(X, high_level); cea(X, high_level)),
 diagnosis(X, hepatitis_A) :- anti_hepatitis_A(X, positive).
 diagnosis(X, hepatitis_B) :- anti_hepatitis_B(X, positive).
 diagnosis(X, hepatitis_C) :- anti_hepatitis_C(X, positive).
-diagnosis(X, cirrhosis) :- alt(X, high), ast(X, high), alp(X, normal), unconjugated_bilirubin(X, high), albumin(X, low).
+diagnosis(X, drug_induced_hepatitis) :- alt(X, high), ast(X, high), alp(X, normal), bilirubin(X, normal), albumin(X, normal);
+                                        eaodt(X, toxic).
+diagnosis(X, hemochromatosis) :- iron_tests(X, hemochromatosis).
+diagnosis(X, alpha_1_antitrypsin_deficiency) :- alpha_1_antirypsin(X, low).
+diagnosis(X, wilson_disease ) :- ceruloplasmin(X, positive), copper_tests(X, positive).
+diagnosis(X, autoimmune_hepatitis_type1) :- ana(X, positive);
+                                            asma(X, positive).
+diagnosis(X, autoimmune_hepatitis_type2) :- anti_lkm1(X, positive).
+
+diagnosis(X, cirrhosis) :- (alt(X, high), ast(X, high), (alp(X, normal); alp(X, high)), bilirubin(X, high), (albumin(X, low); albumin(X, normal)), cbc(X, platelets_low));
+                           liver_biopsy(X, cirrhosis).
+                  
--- Rule_Based/symptoms.pl
+++ Rule_Based/symptoms.pl
@@ -1,4 +1,5 @@
-%SYMPTOMS
+% SYMPTOMS
+%-------------
 
 symptoms(hiatal_hernia, [sharp_abdominal_pain, sharp_chest_pain, nausea, vomiting, difficulty_in_swallowing, heartburn, vomiting_blood]).
 symptoms(gallstone, [sharp_abdominal_pain, upper_abdominal_pain, nausea, vomiting, back_pain, burning_abdominal_pain, sharp_chest_pain]).
@@ -9,9 +10,15 @@ symptoms(pancreatic_cancer, [sharp_abdominal_pain, jaundice, stomach_bloating, n
 symptoms(hepatitis_A, [fatigue, nausea, sharp_abdominal_pain, joint_aches, itching, jaundice, loss_of_appetite, dark_colored_urine, light_colored_stools]).
 symptoms(hepatitis_B, [fatigue, nausea, sharp_abdominal_pain, joint_aches, itching, jaundice, loss_of_appetite, dark_colored_urine, light_colored_stools]).
 symptoms(hepatitis_C, [fatigue, nausea, sharp_abdominal_pain, joint_aches, itching, jaundice, loss_of_appetite, dark_colored_urine, light_colored_stools]).
+symptoms(drug_induced_hepatitis,[fatigue, nausea, sharp_abdominal_pain, joint_aches, itching, jaundice, loss_of_appetite, dark_colored_urine, light_colored_stools]).
+symptoms(inherited_hepatitis,[fatigue, nausea, sharp_abdominal_pain, joint_aches, itching, jaundice, loss_of_appetite, dark_colored_urine, light_colored_stools]).
+symptoms(autoimmune_hepatitis,[fatigue, nausea, sharp_abdominal_pain, joint_aches, itching, jaundice, loss_of_appetite, dark_colored_urine, light_colored_stools]).
+
 symptoms(cirrhosis, [fatigue, nausea, abdominal_discomfort, abdominal_swelling, weakness, itching, jaundice, loss_of_appetite, loss_of_weight]).
 
-
+%-----------------------------------------------------------------------------------------------------------------------
+% PERSONAL SYMPTOMS
+%--------------------
 
 personal_symptoms(ana, [sharp_abdominal_pain, sharp_chest_pain, nausea, heartburn,upper_abdominal_pain]).
 personal_symptoms(anaa, [nausea, itching, joint_aches]).
@@ -19,8 +26,3 @@ personal_symptoms(aca, [nausea, itching, weakness]).
 personal_symptoms(sara, [jaundice, stomach_bloating, nausea]).
 personal_symptoms(peca, [vomiting_blood, upper_abdominal_pain]).
 
-
-contains(S,[]).
-contains(S,[H|T]) :- member(H,S), contains(S,T).
-
-possible_diseases(Name,D) :- personal_symptoms(Name, L),  symptoms(D,L1), contains(L1,L). 
