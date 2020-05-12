e0249db524984d5a8f3b2b9fbc0c90ceb15ec4c7
ananikolasevic <23nikolasevic@gmail.com>
2020-05-12 21:27:14 +0200

    added page for additional tests

--- GastroenterologyApplication/rule_based/patients.pl
+++ GastroenterologyApplication/rule_based/patients.pl
@@ -2,7 +2,7 @@
 
 % Non-steroidal anti-inflammatory drug (nsaid_abuse)
 % Presence of non–type O blood antigens (nt0ba)
-% Body_mass_index (BMI)
+% Body_mass_index (bmi)
 
 %--------------
 
@@ -13,7 +13,7 @@ anamnesis(drug_induced_hepatitis, [abusing_alchohol, aspirin_abuse, steroids_abu
 anamnesis(inherited_hepatitis, [family_history_of_liver_disease]). 
 anamnesis(cirrhosis, [autoimmune_hepatitis, hepatitis_A, hepatitis_B, hepatitis_C, abusing_alchohol, inherited_hepatitis, drug_abuse]).
 anamnesis(gastritis, [genetic_predisposition_gastritis, active_smoking, nsaid_abuse, stress, abusing_alchohol]).
-anamnesis(pancreatic_cancer, [active_smoking, abusing_alchohol,nt0ba, BMI_over_35, family_history_of_pancreatic_cancer]).
+anamnesis(pancreatic_cancer, [active_smoking, abusing_alchohol, nt0ba, bmi_over_35, family_history_of_pancreatic_cancer]).
 anamnesis(ulceraive_colitis, [active_smoking, nsaid_abuse]).
 anamnesis(crohn_disease, [active_smoking, nsaid_abuse, family_history_of_crohn_disease]).
 
--- GastroenterologyApplication/src/App.java
+++ GastroenterologyApplication/src/App.java
@@ -1,44 +1,42 @@
-import java.awt.Component;
 import java.awt.EventQueue;
 import java.awt.Font;
-import java.io.BufferedReader;
-import java.io.FileInputStream;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.Collections;
 
+import javax.swing.AbstractListModel;
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
 import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 
+import com.ugos.jiprolog.engine.JIPAtom;
 import com.ugos.jiprolog.engine.JIPEngine;
+import com.ugos.jiprolog.engine.JIPFunctor;
+import com.ugos.jiprolog.engine.JIPList;
 import com.ugos.jiprolog.engine.JIPQuery;
 import com.ugos.jiprolog.engine.JIPSyntaxErrorException;
 import com.ugos.jiprolog.engine.JIPTerm;
-import com.ugos.jiprolog.engine.JIPTermParser;
 import com.ugos.jiprolog.engine.JIPVariable;
-import javax.swing.JCheckBoxMenuItem;
-import javax.swing.JList;
-import javax.swing.ListCellRenderer;
-import javax.swing.ListModel;
-import javax.swing.AbstractListModel;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import javax.swing.JScrollPane;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import javax.swing.JPanel;
+import javax.swing.JToggleButton;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 
 public class App {
 
 	private JFrame frame;
-
-	/**
-	 * Launch the application.
-	 */
+	JPanel panel = new JPanel();
+	JPanel panelAdditionalTests = new JPanel();
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -64,9 +62,140 @@ public class App {
 	 * Initialize the contents of the frame.
 	 * @throws IOException 
 	 */
-	@SuppressWarnings("unchecked")
 	private void initialize() throws IOException {
+		// initializing frame	
+		frame = new JFrame();
+		frame.setBounds(100, 100, 1006, 568);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+
+		//list of personal symptoms/anamnesis/tests (selected from list)
+		ArrayList<String> personalSymptoms = new ArrayList<String>();
+		ArrayList<String> personalAnamnesis = new ArrayList<String>();
+
+		//lists from prolog
+	    ArrayList<String> listOfSymptoms = consultProlog("symptoms(_, X)");
+	    ArrayList<String> listOfAnamnesis = consultProlog("anamnesis(_, X)");
+
+	    // ------------------------------first panel--------------------------
+	    
+		panelAdditionalTests.setVisible(false);
 		
+		panel.setBounds(0, 0, 992, 521);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNameAndSurname = new JLabel("Name and surname");
+		lblNameAndSurname.setBounds(53, 36, 139, 20);
+		lblNameAndSurname.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		panel.add(lblNameAndSurname);
+		
+		JFormattedTextField formattedTextField = new JFormattedTextField();
+		formattedTextField.setBounds(201, 39, 258, 19);
+		panel.add(formattedTextField);
+
+		JLabel lblSymptoms = new JLabel("Symptoms");
+		lblSymptoms.setBounds(118, 97, 74, 20);
+		lblSymptoms.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		panel.add(lblSymptoms);
+
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(201, 97, 258, 278);
+		panel.add(scrollPane);
+		
+		//setting list of symptoms
+		JList list = new JList();
+		scrollPane.setViewportView(list);
+		list.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		list.setListData((String[]) listOfSymptoms.toArray(new String[0]));
+
+		JLabel lblNewLabel = new JLabel("Anamnesis");
+		lblNewLabel.setBounds(539, 45, 77, 125);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		panel.add(lblNewLabel);
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		scrollPane_1.setBounds(626, 97, 258, 278);
+		panel.add(scrollPane_1);
+		
+		JList list_1 = new JList();
+		scrollPane_1.setViewportView(list_1);
+		list_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		list_1.setListData((String[]) listOfAnamnesis.toArray(new String[0]));
+		
+		JButton btnFindAdditionalTests = new JButton("Find possible diseases");
+		btnFindAdditionalTests.setBounds(441, 455, 191, 29);
+		btnFindAdditionalTests.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				for (Object selected : list.getSelectedValuesList()) {
+					personalSymptoms.add(selected.toString());
+				}
+				for (Object selected : list_1.getSelectedValuesList()) {
+					personalAnamnesis.add(selected.toString());
+				}
+				String person = formattedTextField.getText();
+				String term = "possible_diseases(" + person + "," + personalSymptoms + "," + personalAnamnesis + ", S)";
+	            ArrayList<String> pd = consultProlog(term);
+	          
+				term = "additional_tests(" + person + "," + personalSymptoms + "," + personalAnamnesis + ", T)";
+				ArrayList<String> additional_tests = consultProlog(term);
+	    		additionalTestsPanel(additional_tests);
+			}
+
+
+
+		});
+		btnFindAdditionalTests.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		panel.add(btnFindAdditionalTests);	
+
+	}
+
+	private void additionalTestsPanel(ArrayList<String> additional_tests) {
+		// TODO Auto-generated method stub
+		
+		// ------------------- panel for additional tests--------------------
+		panel.setVisible(false);
+		panelAdditionalTests.setVisible(true);
+		panelAdditionalTests.setBounds(0, 0, 992, 531);
+		frame.getContentPane().add(panelAdditionalTests);
+		panelAdditionalTests.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("Additional tests:");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel_1.setBounds(50, 77, 154, 36);
+		panelAdditionalTests.add(lblNewLabel_1);
+		
+		JScrollPane scrollPane_2 = new JScrollPane();
+		scrollPane_2.setBounds(222, 77, 594, 342);
+		panelAdditionalTests.add(scrollPane_2);
+		
+		JList list_2 = new JList();
+		list_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		scrollPane_2.setViewportView(list_2);
+    	list_2.setListData((String[]) additional_tests.toArray(new String[0]));
+		System.out.println(additional_tests.size());
+
+		ArrayList<String> personalTests = new ArrayList<String>();
+
+		
+		JButton btnNewButton = new JButton("Results");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				for (Object selected : list_2.getSelectedValuesList()) {
+					personalTests.add(selected.toString());
+				}
+				
+			}
+		});
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnNewButton.setBounds(846, 447, 100, 36);
+		panelAdditionalTests.add(btnNewButton);
+		
+	}
+	
+	
+	private ArrayList<String> consultProlog(String term){
 		// New instance of prolog engine
         JIPEngine jip = new JIPEngine();
 
@@ -77,7 +206,11 @@ public class App {
         {
             // consult file
             jip.consultFile("rule_based/symptoms.pl");
-            queryTerm = jip.getTermParser().parseTerm("symptoms(_, X)");
+            jip.consultFile("rule_based/tests.pl");
+            jip.consultFile("rule_based/patients.pl");
+
+
+            queryTerm = jip.getTermParser().parseTerm(term);
         }
         catch(JIPSyntaxErrorException ex)
         {
@@ -90,74 +223,50 @@ public class App {
         JIPTerm solution;
 
         ArrayList<String> listOfSymptoms = new ArrayList<String>();
-        
+        ArrayList<String> listOfAnamnesis = new ArrayList<String>();
+        ArrayList<String> returnList = new ArrayList<String>();
+ 
         // Loop while there is another solution
         while (jipQuery.hasMoreChoicePoints())
         {
+        	
             solution = jipQuery.nextSolution();
-            System.out.println(solution);
+            if(solution!=null) {
+            System.out.println("solution: " + solution);
             
             JIPVariable[] vars = solution.getVariables();
             for (JIPVariable var : vars) {
                 if (!var.isAnonymous()) {
-                    System.out.print(var.getName() + " = " + var.toString(jip) + " ");
-                    System.out.println();
-                    String[] symptoms = var.toString(jip).substring(1, var.toString(jip).length()-1).split(",");
-                    for (String symptom : symptoms) {
-                    	if(!listOfSymptoms.contains(symptom)) {
-                    		listOfSymptoms.add(symptom);
-                    	}
-					}
+                    System.out.println(var.getName() + " = " + var.toString(jip) + " ");
+                    if(solution.toString(jip).contains("symptoms")) {
+	                    String[] symptoms = var.toString(jip).substring(1, var.toString(jip).length()-1).split(",");
+	                    for (String symptom : symptoms) {
+	                    	if(!listOfSymptoms.contains(symptom)) {
+	                    		listOfSymptoms.add(symptom);
+	                    		returnList = listOfSymptoms;
+	                    	}
+						}
+                    } else if(solution.toString(jip).contains("anamnesis")) {
+	                    String[] anamnesis = var.toString(jip).substring(1, var.toString(jip).length()-1).split(",");
+	                    for (String a : anamnesis) {
+	                    	if(!listOfAnamnesis.contains(a)) {
+	                    		listOfAnamnesis.add(a);
+	                    		returnList = listOfAnamnesis;
+	                    	}
+						}    
+                    } else if(solution.toString(jip).contains("possible_diseases")) {
+                    	System.out.println("Possible disease: " + var.toString(jip));
+                    	returnList.add(var.toString(jip));
+                    } else if(solution.toString(jip).contains("additional_tests")) {
+                    	System.out.println("Additional tests: " + var.toString(jip));
+                    	returnList.add(var.toString(jip));
+                    }
                 }
             }
+        	}
         }
-        
-		
-		frame = new JFrame();
-		frame.setBounds(100, 100, 626, 398);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
-		
-		JLabel lblNameAndSurname = new JLabel("Name and surname");
-		lblNameAndSurname.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblNameAndSurname.setBounds(10, 10, 146, 32);
-		
-		frame.getContentPane().add(lblNameAndSurname);
-		JFormattedTextField formattedTextField = new JFormattedTextField();
-		formattedTextField.setBounds(166, 19, 397, 19);
-		frame.getContentPane().add(formattedTextField);
-		
-		JLabel lblSymptoms = new JLabel("Symptoms");
-		lblSymptoms.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblSymptoms.setBounds(10, 57, 146, 19);
-		frame.getContentPane().add(lblSymptoms);
-		
-		// list of symptoms
-		JList list = new JList();
-		list.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		list.setModel(new AbstractListModel() {
-			String [] values = (String[]) listOfSymptoms.toArray(new String[0]);
-			public int getSize() {
-				return values.length;
-			}
-			public Object getElementAt(int index) {
-				return values[index];
-			}
-		});
-		list.setBounds(166, 48, 397, 250);
-		frame.getContentPane().add(list);
-
-		
-		JButton btnFindAdditionalTests = new JButton("Find additional tests");
-		btnFindAdditionalTests.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-							}
-		});
-		btnFindAdditionalTests.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		btnFindAdditionalTests.setBounds(386, 308, 177, 21);
-		frame.getContentPane().add(btnFindAdditionalTests);
-	}
+		System.out.println("-------------------------------------------");
+        return returnList;
+     }
 	
-
-    
 }
